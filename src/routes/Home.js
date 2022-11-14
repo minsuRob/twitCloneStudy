2f7105a3687624d2826f619398fbbe5b0a212f56
minsu.rob <minsu.rob@gmail.com>
2022-11-14 23:20:19 +0900

    일괄 데이터 불러오기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
 
-    const onSubmit = (event) => {
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((dbNweet) =>
+                setNweets((prev) => [dbNweet.data(), ...prev])
+            )
+    }
+
+    useEffect(() => {
+        getNweets();
+    }, []);
+
+    console.log(nweets);
+
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("nweets").add({
+            text: nweet,
+            createdAt: Date.now(),
+        });
+        setNweet("");
     }
 
     const onChange = (event) => {
@@ -17,9 +37,15 @@ const Home = () => {
 
     return (
         <form onSubmit={onSubmit}>
-            <input value={nweet} onChange={onchange} type="text" placeholder="What's on your mind?" maxLength={120}/>
-            <input type="submit" value="Nweet"/>
-        </form>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
     )
 }
 
